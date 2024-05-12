d1a5792b9d246b642df473cd0b5dfcc777696e21
beetlepig <devpig@icloud.com>
2024-05-12 13:19:27 -0500

    use node runtime

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ import { Fragment, type ReactNode, useMemo } from "react";
 import SearchBar from "~/components/organisms/search-bar";
 import stylesheet from "~/tailwind.css?url";
 
-export const config = { runtime: "edge" };
+export const config = { runtime: "nodejs" };
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesheet }];
 export const loader = ({ request }: LoaderFunctionArgs) => {
